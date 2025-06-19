b0221e7ebb13f3e96a5973a7c6a6755288b43048
Max <eastsea1020n@gmail.com>
2025-06-19 00:50:47 -0700

    remove vite base monorepo

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import MyEditorComponent from "./MyEditor";
-
-export default function App() {
-  const [value, setValue] = useState("");
-
-  return <MyEditorComponent value={value} onValueChange={setValue} />;
-}
